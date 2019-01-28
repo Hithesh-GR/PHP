93b08ff444b2f8b1dbf795bca8d98ec8286fa1a2
Hithesh-GR <hithu.gowda2@gmail.com>
2019-01-28 15:54:39 +0530

    first commit

---
+++ AlgorithmPrograms/utility.php
@@ -0,0 +1,16 @@
+<?php
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> php fileName.php                 
+ *  Purpose         : TO deploy all the business logic.
+ *  @file           : utility.php
+ *  @overview       : ALl the business logic bus be here.
+ *  @server         : Apache2 HTTP server
+ *  @author         : HITHESH G R
+ *  @version        : PHP v7.0.32
+ *  @since          : 28-01-2019
+ ******************************************************************************/
+class Utility
+{
+    
+}
+?>

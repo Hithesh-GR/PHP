b988e88e1eea4ace41a2859b19bd026f22e5c432
Hithesh-GR <hithu.gowda2@gmail.com>
2019-01-28 18:49:21 +0530

    first commit

--- AlgorithmPrograms/anagramString.php
+++ AlgorithmPrograms/anagramString.php
@@ -1,11 +1,17 @@
 <?php
-/*************************************************************************************************
- *  @Purpose        :    
+/***********************************************************************************
+ *  @Purpose        : check the two strings are same or not   
  *  @file           : anagramString.php
- *  @overview       : 
+ *  @overview       : take two string from user and compare those, and prints the 
+                      message wheather they are anagram or not.
  *  @author         : HITHESH G R
  *  @version        : PHP v7.0.32
  *  @since          : 28-01-2019
- ***************************************************************************************************/
-echo "My first PHP script!\n";
+ ***********************************************************************************/
+include('utility.php');
+    echo "enter first string: ";
+    $str1 = Utility::readString();//read string1
+    echo "enter second string: ";
+    $str2 = Utility::readString();//read string2
+    Utility::checkAnagram($str1,$str2);
 ?>
--- AlgorithmPrograms/utility.php
+++ AlgorithmPrograms/utility.php
@@ -11,6 +11,43 @@
  ******************************************************************************/
 class Utility
 {
-    
+    /**
+    * @purpose : function to read string and return string value
+    */
+    public static function readString() {
+        $var = readline("");
+        while (is_numeric($var)) { 
+            /**
+             * if input is numeric then throw error message
+            */
+            echo "enter valid input: ";
+            fscanf(STDIN, "%s", $var);
+        }
+        return $var;
+    }
+    /**
+    * @purpose : check the two strings are same or not
+    * @parameter: string1-first string of user input 
+                   string2-second string of user input 
+    * @description : take two string from user and compare those, and prints the 
+                      message wheather they are anagram or not.
+    */
+    public static function checkAnagram($str1, $str2)
+    {
+        if (count($str1) == count($str2)) { //check length of two string are equal
+            $str1Arr = str_split($str1); // split to array
+            $str2Arr = str_split($str2); //split to array
+            asort($str1Arr); //sort array by values
+            asort($str2Arr); //sort array by values
+            $str3 = implode("", $str1Arr); //array to string by ""
+            $str4 = implode("", $str2Arr); //array to string by ""
+            if ($str3 == $str4) { //check both string are equal
+                echo "-->Anagram \n";
+            } 
+            else {
+                echo "-->Not Anagram\n";
+            }
+        }
+    }
 }
 ?>
